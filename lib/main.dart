ce2a71a0efd295c2c1b73c4939cffac4e3e04040
RaulMillanMoreno <raul.millan@sarria.salesians.cat>
2024-11-03 11:12:43 +0100

    añadir tipos de dialogos
    se han añadidos 4 formas de dialogo, las cuales, solo una es funcional pero las otras están comentadas para poder ser usadas.

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Recuperar el valor d''un camp de text',
+      theme: ThemeData(       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      
       home: MyCustomForm(),
     );
   }
@@ -44,12 +49,78 @@ class _MyCustomFormState extends State<MyCustomForm> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('this is a SnackBar'),
-              duration: Duration(seconds: 4),            
-            ),
-          );
+          showModalBottomSheet(
+            backgroundColor: Colors.transparent,
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                decoration: const BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ),
+                ),
+                height: 200,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text('Modal Bottom Sheet'),
+                      ElevatedButton(
+                        onPressed: () => Navigator.of(context).pop(), 
+                        child: const Text('close bottomsheet'),
+                        )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );  
+
+          // showDialog(context: context,
+          //   barrierDismissible: true, 
+          //   builder: (BuildContext context){
+          //     return SimpleDialog(
+          //       title: const Text('Hola'),
+          //       children: [
+          //         SimpleDialogOption(
+          //           onPressed: () {
+          //             Navigator.of(context).pop();
+          //           },
+          //           child: const Text('opcion 1'),
+          //         ),
+          //       ],
+          //     );
+          //   },          
+          // );
+
+          // showDialog(context: context,
+          //   barrierDismissible: true, 
+          //   builder: (BuildContext context){
+          //     return AlertDialog(
+          //       title: const Text('Warning'),
+          //       content: const Text('peligro: no pulses el boton'),
+          //       actions: [
+          //         ElevatedButton(
+          //           onPressed: () {
+          //             Navigator.of(context).pop();
+          //           },
+          //           child: const Text('OK'),
+          //         ),
+          //       ],
+          //     );
+          //   },          
+          // );   
+                     
+          // ScaffoldMessenger.of(context).showSnackBar(
+          //   const SnackBar(
+          //     content: Text('this is a SnackBar'),
+          //     duration: Duration(seconds: 4),            
+          //   ),
+          // );
+
         },
         child: const Icon(Icons.text_fields),
       ),
